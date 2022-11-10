cc650d9bdd5598a3f3b5dcfed0fd5250d94e490b
Ly <2371021351@qq.com>
2022-11-10 08:54:46 +0800

    first

--- src/main/java/zhangwenhui/gittest.java
+++ src/main/java/zhangwenhui/gittest.java
@@ -3,5 +3,7 @@ package zhangwenhui;
 public class gittest {
     public static void main(String[] args){
         System.out.println("hello zhangwenhui");
+        System.out.println("hello zhangwenhui");
+        System.out.println("hello zhangwenhui");
     }
 }
